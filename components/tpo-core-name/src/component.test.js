e465d11b495762d57f998d2bd9790286aad5a93e
John Clark <socrates37@gmail.com>
2016-11-02 15:59:45 -0600

    added karma config and test tests

--- components/tpo-core-name/src/component.test.js
+++ components/tpo-core-name/src/component.test.js
@@ -1,3 +1,17 @@
+const {expect} = require('chai');
+
+let element;
+
 describe('tpo-core-name', () => {
-  it('should work');
+  beforeEach(() => {
+    document.body.insertAdjacentHTML('beforeend',
+      '<tpo-core-name></tpo-core-name>');
+    element = document.body.lastChild;
+  });
+  afterEach(() => {
+    document.body.removeChild(element);
+  });
+  it('should work', () => {
+    expect(element.innerHTML).to.equal('hello!');
+  });
 });
