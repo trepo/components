60404b50dd38d95c107c573c6f32e2111113f93d
John Clark <socrates37@gmail.com>
2016-11-10 16:00:58 -0700

    hooked up event listeners

--- components/tpo-core-name/src/component.js
+++ components/tpo-core-name/src/component.js
@@ -2,13 +2,15 @@ require('tpo-edit-bar');
 const core = require('tpo-mixins/core.js');
 const shadow = require('tpo-mixins/shadow.js');
 
-class CoreName extends shadow(core(HTMLElement)) {
+class CoreName extends core(shadow(HTMLElement)) {
   constructor() {
     super({
       template: 'tpo-core-name',
       attributes: CoreName.observedAttributes,
       $: {
         toggle: '#toggle',
+        name: '#name',
+        bar: '#edit-bar',
       },
     });
   }
@@ -24,13 +26,36 @@ class CoreName extends shadow(core(HTMLElement)) {
   connectedCallback() {
     super.connectedCallback();
 
-    this.$.toggle.addEventListener('click', (e) => {
-      console.log('toggling', e.target);
-    });
+    this.$.toggle.addEventListener('click', (e) => this._toggle());
+
+    this.$.name.addEventListener('input', (e) => this.$.bar.changed());
+
+    this.$.bar.addEventListener('create', (e) => this._create());
+    this.$.bar.addEventListener('update', (e) => this._update());
+    this.$.bar.addEventListener('delete', (e) => this._delete());
+  }
+
+  _toggle() {
+    console.log('toggle');
   }
 
   _nodeChanged(newVal, oldVal) {
     console.log('nodeChanged', newVal, oldVal);
+  }
+
+  _create() {
+    console.log('create');
+    this.$.bar.created();
+  }
+
+  _update() {
+    console.log('update');
+    this.$.bar.updated();
+  }
+
+  _delete() {
+    console.log('delete');
+    this.$.bar.deleted();
   }
 }
 
